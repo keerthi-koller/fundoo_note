46e80f6b5230376e94392afdb6facaf9ae1c5f99
keerthi-koller <keerthi.nk8@gmail.com>
2023-12-15 11:59:53 +0530

    password validation done for registering the user if password mismatches it won't save data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,19 +3,56 @@ import * as UserService from '../services/user.service';
 
 export const newUser = async (req, res, next) => {
   const data = await UserService.newUser(req.body);
-  try {
-    res.status(HttpStatus.CREATED).json({
-      code: HttpStatus.ACCEPTED,
-      data: data,
-      message: 'User Created Successfully'
-    });
-  } catch (error) {
-    res.status(HttpStatus.NOT_FOUND).json({
-      code: HttpStatus.NOT_FOUND,
-      data: data,
-      message: 'User Not Saved'
-    });
-    next(error);
+  if (data === "Password mismatch!!")
+  {
+    try {
+      res.status(HttpStatus.NOT_ACCEPTABLE).json({
+        code: HttpStatus.NOT_ACCEPTABLE,
+        data: data,
+        message: 'Password missmatch'
+      });
+    } catch (error) {
+      res.status(HttpStatus.NOT_ACCEPTABLE).json({
+        code: HttpStatus.NOT_ACCEPTABLE,
+        data: data,
+        message: 'User Not Saved'
+      });
+      next(error);
+    }
+  }
+  else if (data === "User Already Exists")
+  {
+    try {
+      res.status(HttpStatus.NOT_ACCEPTABLE).json({
+        code: HttpStatus.NOT_ACCEPTABLE,
+        data: data,
+        message: 'User Already Exists'
+      });
+    } catch (error) {
+      res.status(HttpStatus.NOT_ACCEPTABLE).json({
+        code: HttpStatus.NOT_ACCEPTABLE,
+        data: data,
+        message: 'User Not Saved'
+      });
+      next(error);
+    }
+  }
+  else
+  {
+    try {
+      res.status(HttpStatus.CREATED).json({
+        code: HttpStatus.ACCEPTED,
+        data: data,
+        message: 'User Created Successfully'
+      });
+    } catch (error) {
+      res.status(HttpStatus.NOT_FOUND).json({
+        code: HttpStatus.NOT_FOUND,
+        data: data,
+        message: 'User Not Saved'
+      });
+      next(error);
+    }
   }
 };
 
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,36 +4,26 @@ export const newUser = async (body) => {
   const d = await User.findOne({emailId: body.emailId});
 
   if (d === null) {
-    const data = await User.create(body);
-    return data;
+    if (body.password === body.confirmPassword) {
+      const data = await User.create(body);
+      return data;
+    }
+    else {
+      return "Password mismatch!!";
+    }
   } else {
     return 'User Already Exists';
   }
 };
 
 export const loginUser = async (emailId, password) => {
-
-console.log(emailId+"lll");  
-console.log(password+"ppp");
-
-  const data = await User.findOne({emailId});
-
-  console.log(data);
-
-  if (data === null)
-{
+const data = await User.findOne({emailId});
+if (data === null) {
   return "User doesn't exists.."
 }
-else
-{
-  if (password == data.password)
-  {
-    return "User LoggedIn Successfully!!"
-  }
-  else
-  {
-    return "Password is Incorrect!"
-  }
+else {
+  let r = password == data.password ? "User LoggedIn Successfully!!" : "Password is Incorrect!";
+  return r;
 }
 
 };
